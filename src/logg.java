84255b3392ded25bf0f18a30c089a9796c8ed21f
te17adrian <adrian.lidgren@elev.ga.ntig.se>
2019-08-27 11:07:31 +0200

    Added scanner for user input.

--- src/logg.java
+++ src/logg.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
+
 public class logg {
-    
+    public static void main(String[] args) {
+        System.out.println("Vad vill du göra?" + "\n" + "1: Visa loggboken" + "\n" + "2: Skriv in en ny post i loggboken" +"\n" + "6. Avsluta" );
+        Scanner input = new Scanner(System.in);
+        String answer = input.nextLine();
+
+
+        if (answer.equals("1")) {
+            System.out.println("test1");
+        } else if (answer.equals("2")) {
+            System.out.println("test2");
+        } else if (answer.equals("6")) {
+            System.out.println("test6");
+            System.exit(0);
+        }
+    }
 }
